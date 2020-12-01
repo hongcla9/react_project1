d60154b0a35bd1b23ad38d7e5fa0be6082d53079
hongcla9 <hongcla9@gmail.com>
2020-12-01 17:51:38 +0900

    add

--- server.js
+++ server.js
@@ -1,32 +1,68 @@
-const fs =require('fs');
+const fs = require('fs');
+
 const express = require('express');
+
 const bodyParser = require('body-parser');
+
 const app = express();
+
 const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
 const data = fs.readFileSync('./database.json');
+
 const conf = JSON.parse(data);
+
 const mysql = require('mysql');
 
+
+
+const multer = require('multer');
+
+const upload = multer({dest: './upload'})
+
+
+
 const connection = mysql.createConnection({
-    host: conf.host,
-    user : conf.user,
-    password :conf.password,
-    port : conf.port,
-    database:conf.database
+
+host: conf.host,
+
+user: conf.user,
+
+password: conf.password,
+
+port: conf.port,
+
+database: conf.database
+
 });
+
 connection.connect();
 
+
+
 app.get('/api/customers', (req, res) => {
-   connection.query(
-    "SELECT * FROM CUSTOMER",
-       (err,rows,fields) => {
-           res.send(rows);
-       }
-   );
-    
+
+connection.query(
+
+'SELECT * FROM CUSTOMER',
+
+(err, rows, fields) => {
+
+res.send(rows);
+
+}
+
+)
+
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+app.use('/image', express.static('./upload'));
+
