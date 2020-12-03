72ace94b3915bc689a3ed0ca750cf2ac3efaa347
hongcla9 <hongcla9@gmail.com>
2020-12-03 17:51:42 +0900

    server 재시작

--- server.js
+++ server.js
@@ -1,36 +1,15 @@
-const fs = require('fs');
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-Parser');
+const app = express()
 const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const data = fs.readFileSync('./database.json');
-const conf = JSON.parse(data);
-const mysql = require('mysql');
-const multer = require('multer');
-const upload = multer({dest: './upload'})
-const connection = mysql.createConnection({
-host: conf.host,
-user: conf.user,
-password: conf.password,
-port: conf.port,
-database: conf.database
+app.use (bodyParser.json());
+app.use (bodyParser.urlencoded({ extended:true }))
+
+app.get('/api/hello', (req, res) => {
+  res.send({message: 'Hello Express'});
 });
 
-connection.connect();
-
-
-
-app.get('/api/customers', (req, res) => {
-connection.query(
-'SELECT * FROM CUSTOMER',
-(err, rows, fields) => {
-res.send(rows);
-}
-)
-});
-
-app.use('/image', express.static('./upload'));
-
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
